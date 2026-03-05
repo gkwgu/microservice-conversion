e7b3191a749032b650a79931b9fae71d7ec5176c
gkwgu <polina.petr0v4@yandex.ru>
2026-03-05 17:27:02 +0300

    first commit

---
+++ src/test/java/com/conversion/microservice_conversion/MicroserviceConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conversion.microservice_conversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceConversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
